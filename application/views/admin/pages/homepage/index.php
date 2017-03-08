f7822b44dc281ea13d88939271d214f26b4ed5e9
John Robert Jerodiaz (Roy) <royfdc@gmail.com>
2017-03-08 18:07:49 +0800

    ADD: Data table of overdue list in homepage

--- application/views/admin/pages/homepage/index.php
+++ application/views/admin/pages/homepage/index.php
@@ -34,41 +34,45 @@ $(document).ready(function(){
                             Overdue Customer
                         </div>
                         <div class="panel-body">
-                            <table class="table table-bordered">
-                                <tr>
-                                    <th>No.</th>
-                                    <th>Name</th>
-                                    <th>Meter No</th>
-                                    <th>Amount</th>
-                                    <th>Reading Date</th>
-                                    <th></th>
-                                </tr>
-                                <?php 
-                                $counter = 1;
-                                foreach($overdue as $row) {
-                                    $this->db->where('id', $row->customer_id);
-                                    $query = $this->db->get('customers');
-                                    $customer = $query->row();
-                                ?>
-                                <tr>
-                                    <td><?php echo $counter++; ?></td>
-                                    <td>
-                                        <?php echo ucwords(strtolower($customer->customer_firstname.' '.$customer->customer_lastname));?>
-                                    </td>
-                                    <td>
-                                        <?php echo $customer->customer_meter_no; ?>
-                                    </td>
-                                    <td>
-                                        <?php echo $row->customer_reading_amount; ?>
-                                    </td>
-                                    <td>
-                                        <?php echo $row->customer_reading_date;?>
-                                    </td>
-                                    <td>
-                                        <a href="<?php echo base_url();?>index.php/admin/BillingController/singleReport/<?php echo $customer->id;?>" target="blank">View Reports</a>
-                                    </td>
-                                </tr>
-                                <?php } ?>
+                            <table class="table table-bordered" id="overdue-datatable-info">
+                                <thead>
+                                    <tr>
+                                        <th>No.</th>
+                                        <th>Name</th>
+                                        <th>Meter No</th>
+                                        <th>Amount</th>
+                                        <th>Reading Date</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php 
+                                    $counter = 1;
+                                    foreach($overdue as $row) {
+                                        $this->db->where('id', $row->customer_id);
+                                        $query = $this->db->get('customers');
+                                        $customer = $query->row();
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $counter++; ?></td>
+                                        <td>
+                                            <?php echo ucwords(strtolower($customer->customer_firstname.' '.$customer->customer_lastname));?>
+                                        </td>
+                                        <td>
+                                            <?php echo $customer->customer_meter_no; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $row->customer_reading_amount; ?>
+                                        </td>
+                                        <td>
+                                            <?php echo $row->customer_reading_date;?>
+                                        </td>
+                                        <td>
+                                            <a href="<?php echo base_url();?>index.php/admin/BillingController/singleReport/<?php echo $customer->id;?>" target="blank">View Reports</a>
+                                        </td>
+                                    </tr>
+                                    <?php } ?>
+                                </tbody>
                             </table>
                         </div>
                     </div>
@@ -85,3 +89,11 @@ $(document).ready(function(){
         </div><!--/row-->
     </div>
 </div><!--/.container-->
+
+<script>
+    $(document).ready(function(){
+        $('#overdue-datatable-info').DataTable({
+            responsive: true
+        });
+    });
+</script>
